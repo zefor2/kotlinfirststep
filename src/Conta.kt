966130ebee586a1abeb57386927d04ef94ee19f2
José Dias <zefor2@gmail.com>
2020-07-22 15:55:11 -0300

    add depositar method

--- src/Conta.kt
+++ src/Conta.kt
@@ -2,4 +2,9 @@ class Conta {
     var titular = ""
     var numero = 0
     var saldo = 0.0
+
+    fun depositar(saldo: Double){
+        println("Depositando na conta do ${this.titular}")
+        this.saldo = saldo
+    }
 }
