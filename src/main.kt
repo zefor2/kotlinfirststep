e4bcb5a9a12eca7b63dbfd296f9fd0827496d12c
José Dias <zefor2@gmail.com>
2020-07-22 14:51:47 -0300

    add conditional struct

--- src/main.kt
+++ src/main.kt
@@ -5,8 +5,17 @@ fun main(){
     var saldo = 0.0
     saldo = 100 + 2.0
     saldo += 200
+    saldo -= 400
 
     println("titular $titular")
     println("número da conta $numeroConta")
     println("saldo da conta $saldo")
+
+
+    when{
+        saldo > 0.0 ->  println("conta é positiva")
+        saldo == 0.0 ->  println("conta é neutra")
+        saldo < 0.0 ->  println("conta é negativa")
+    }
+
 }
